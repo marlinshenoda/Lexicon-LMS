845b336264d1ab11fb7679a0fe13110ffe1ccf0d
HP <HP@PC>
2022-09-08 17:04:15 +0200

    Adding Activity Type & Activity classes to the Entities with it's properties

---
+++ Lexicon-LMS.Core/Entities/ActivityType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexicon_LMS.Core.Entities
+{
+#nullable disable
+    public class ActivityType
+    {
+        public int ActivityTypeId { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string ActivityTypeName { get; set; } = String.Empty;
+
+        //Navigation Properties
+        public ICollection<Activity> Activitys { get; set; } = new List<Activity>();
+
+    }
+}
