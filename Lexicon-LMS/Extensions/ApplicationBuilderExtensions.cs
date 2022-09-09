853378b48a2f8cac86d73b1570ce68badd53e433
marlinshenoda <105081672+marlinshenoda@users.noreply.github.com>
2022-09-09 13:24:21 +0200

    seed

---
+++ Lexicon-LMS/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Lexicon_LMS.Data;
+
+namespace Lexicon_LMS.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static async Task<IApplicationBuilder> SeedDataAsync(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                var db = serviceProvider.GetRequiredService<ApplicationDbContext>();
+
+                //db.Database.EnsureDeleted();
+                //db.Database.Migrate();
+
+         
+            }
+
+            return app;
+
+        }
+    }
+}
