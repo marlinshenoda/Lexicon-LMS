bdfecfecb418928f7baed6cfe88ebb3da79b4574
HP <HP@PC>
2022-09-09 09:51:52 +0200

    Finish UserEntities

--- Lexicon-LMS.Core/Entities/User.cs
+++ Lexicon-LMS.Core/Entities/User.cs
@@ -1,7 +1,16 @@
-﻿namespace Lexicon_LMS.Core.Entities
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Lexicon_LMS.Core.Entities
 {
-    public class User
+    public class User : IdentityUser
     {
+        public String FirstName { get; set; } = String.Empty;
+        public String LastName { get; set; } = String.Empty;   
+        public String FullName => FirstName + LastName;
+
+        public int CourseId { get; set; }
+        public ICollection<Document> Documents { get; set; } = new List<Document>();
+        
 
     }
 }
