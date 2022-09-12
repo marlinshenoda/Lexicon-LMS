4d25ed8b6bafe0c775dd955dc87d139f9121026b
Rikard Persson <Rikard.Persson92@hotmail.com>
2022-09-12 09:20:29 +0200

    upp

---
+++ Lexicon-LMS.Data/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Lexicon_LMS.Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lexicon_LMS.Data
+{
+
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
