029ca5f7639905b77a4d82589a1729619bd2c024
Rikard Persson <Rikard.Persson92@hotmail.com>
2022-09-15 13:34:39 +0200

    Update

---
+++ Lexicon-LMS.Core/Entities/ViewModel/ActivityListViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexicon_LMS.Core.Entities.ViewModel
+{
+    public class ActivityListViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [DisplayName("Start Time")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:d}")]
+        public DateTime StartDate { get; set; }
+        [DisplayName("End Time")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:d}")]
+        public DateTime EndDate { get; set; }
+        [DisplayName("Activity Type")]
+        public string ActivityType { get; set; }
+
+        public string? ModelName { get; set; }
+
+    }
+}
