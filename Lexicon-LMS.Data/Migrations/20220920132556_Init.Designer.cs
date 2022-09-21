e5382d73a4a78533662caaefea08478ff1fba4d0
Rikard Persson <Rikard.Persson92@hotmail.com>
2022-09-21 09:16:05 +0200

    remove

--- Lexicon-LMS.Data/Migrations/20220920132556_Init.Designer.cs
+++ Lexicon-LMS.Data/Migrations/20220920132556_Init.Designer.cs
@@ -12,13 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Lexicon_LMS.Data.Migrations
 {
     [DbContext(typeof(Lexicon_LMSContext))]
-<<<<<<<< HEAD:Lexicon-LMS.Data/Migrations/20220920132556_Init.Designer.cs
-    [Migration("20220920132556_Init")]
-    partial class Init
-========
     [Migration("20220920113656_init")]
-    partial class init
->>>>>>>> master:Lexicon-LMS.Data/Migrations/20220920113656_init.Designer.cs
+    partial class init    
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
