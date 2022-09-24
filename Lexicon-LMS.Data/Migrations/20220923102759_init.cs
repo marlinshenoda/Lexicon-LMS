b46cdc80c492b75dc6f9eb199df5c4d997564d59
Adam Daa <adamdaa87@gmail.com>
2022-09-24 17:31:20 +0200

    Welcome Page changes 2

--- Lexicon-LMS.Data/Migrations/20220923102759_init.cs
+++ Lexicon-LMS.Data/Migrations/20220923102759_init.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Lexicon_LMS.Data.Migrations
 {
-    public partial class Init : Migration
+    public partial class init : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
